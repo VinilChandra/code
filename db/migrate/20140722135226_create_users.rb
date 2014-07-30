fc077c6a8beb5b01bee21b3594c8b9ca6ebfb9ec
VinilChandra <vinilchandraranga@gmail.com>
2014-07-30 13:24:56 +0530

    Update 20140722135226_create_users.rb

--- db/migrate/20140722135226_create_users.rb
+++ db/migrate/20140722135226_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.integer :phone
+      t.bigint :phone
       t.text    :address
       t.string :state
       t.string  :dist
